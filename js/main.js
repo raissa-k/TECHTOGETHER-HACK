f50654606352f54f12a77da35a8639a54cdecafb
raissa-k <raissakuzerdev@gmail.com>
2022-02-26 18:07:17 -0300

    Added placeholder calc to 403b. Fixed phone sizing

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ document.getElementById('403ListenButton').onclick = function(){
     speechSynthesis.speak(utterance403b);
 };
 
+document.getElementById('MiddleListenButton').onclick = function(){
+    var text = document.getElementById('homeMiddleParagraph').innerText;
+    utterance403b = new SpeechSynthesisUtterance();
+    utterance403b.text = text;
+    speechSynthesis.speak(utterance403b);
+};
+
+
 document.getElementById('zoomInButton').onclick = function() {
 	x=document.querySelector('html')
     x.style.fontSize= '120%';
