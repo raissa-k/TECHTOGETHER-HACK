fc0943c2e90cdf85a1e7f1de54060871e338197f
raissa-k <raissakuzerdev@gmail.com>
2022-02-27 01:04:24 -0300

    Added calculator to 403b. Changed nav bar

--- js/403b.js
+++ js/403b.js
@@ -5,10 +5,6 @@ document.getElementById('401ListenButton').onclick = function(){
     speechSynthesis.speak(utterance401k);
 };
 
-document.getElementById('403BButton').onclick = function(){
-    document.getElementById('401kCalcResults').style.display = 'block'
-};
-
 document.getElementById('zoomInButton').onclick = function() {
 	x=document.querySelector('html')
     x.style.fontSize= '120%';
@@ -24,8 +20,7 @@ document.getElementById('zoomNormalButton').onclick = function() {
     x.style.fontSize= '62.5%';
 }
 
-
-document.querySelector('#compute').addEventListener('click',amount401k)
+/* document.querySelector('#compute').addEventListener('click',amount401k)
 
 function amount401k(){
     const currentBalance = document.querySelector('#value401k').value
@@ -42,8 +37,74 @@ function amount401k(){
     value = value + currentBalance * estgrowth
     document.querySelector('#placeForValue').innerText = value
     document.getElementById('401kCalcResults').style.display = 'block' 
+} */
+
+document.querySelector('#compute').addEventListener('click',amount401k)
+
+function amount401k(){
+    const currentBalance = document.querySelector('#value401k').value
+    const d = document.querySelector('#monthlycont').value //payment value//
+    const k = 12; //compounding period//
+    const catchup = document.querySelector('#catchupamount').value/100 //extra contribution amount after 50//
+    const currentage = document.querySelector('#currentage').value 
+    const rtmtage = document.querySelector('#rtmtage').value
+    const r = document.querySelector('#annualrate').value/100 
+    const N = rtmtage - currentage
+    const catchupage = 50-currentage
+    const intrate= Math.pow((1 + r/k) , N*k)
+    const numerator = d * (intrate - 1)
+    const denominator = r/k
+   // const growth = 0.05
+    //const estgrowth = Math.pow((1 + growth/k),k*N)
+//catchup is additional perecentage want to add up to 540 more a month//
+    if(catchupage >= "0" && catchup !== "" && catchup !== '0'){
+        const catchupN = rtmtage - 50  //catchupN limit in 20.5yrs//
+        const catchuppmt = 541.666667*catchup //original payment plus percentage of catchup pmt//
+        let intratecatchup = Math.pow((1+r/k), catchupN*k)
+        let numeratorcatchup =  catchuppmt * (intratecatchup - 1)
+        let catchupperiodsvalue = numeratorcatchup/ denominator
+       // let otherperiodN = N - catchupN  
+       // let otherperiodintrate = Math.pow((1 + r/k), otherperiodN*k)
+       // let otherperiodnumerator = d * (otherperiodintrate -1)
+        let otherperiodsvalue = numerator / denominator
+        let value = catchupperiodsvalue + otherperiodsvalue + currentBalance
+        document.querySelector('#placeForValue').innerText = value
+        document.getElementById('401kCalcResults').style.display = 'block' 
+
+    } else if ( catchup === "" || catchup === "0"){
+        let value = numerator / denominator
+        value = (value + currentBalance) 
+         document.querySelector('#placeForValue').innerText = value 
+    }
 }
 
-document.querySelector('#reset').onclick = function() {
+document.querySelector('#estimate').addEventListener('click', calculate)
+function calculate() {
+
+    //Get values
+    const valueMonthly = document.querySelector('#valueMonthly').value
+    const growthInput = document.querySelector('#growthInput').value
+    const retAge = document.querySelector('#retAge').value
+    const lifeAge = document.querySelector('#lifeAge').value
+
+    //Calculate for Growth Percentage and Number of years withdrawing money
+    let growthP = growthInput / 100
+    let withdrawYrs = lifeAge - retAge
+
+    //Calculation for Payout
+    // let numerator = valueMonthly * (1 - (1 + (growthP/12)) ** (-(withdrawYrs * 12)))
+    let numerator = valueMonthly * (1 - (1 + (growthP/12)) ** (-(withdrawYrs * 12)))
+    let denominator = growthP / 12
+    let retirementAmount = numerator / denominator
+    document.querySelector('#placeForEst').innerText = retirementAmount
+    document.getElementById('401kProjResults').style.display = 'block' 
+}
+
+
+document.querySelector('#reset401Calc').onclick = function() {
     document.getElementById('form401kBalance').reset()
+}
+
+document.querySelector('#reset401Projection').onclick = function() {
+    document.getElementById('form401kEstimate').reset()
 }
